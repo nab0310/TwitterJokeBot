3ec081ffbfc25f45c9dc74b68c2810f53f213570
Nick Behrens <behrensn@iastate.edu>
2016-12-13 16:53:32 -0600

    Initial Commit

---
+++ index.js
@@ -0,0 +1,53 @@
+console.log('Its alive');
+
+var twit = require('twit');
+
+var keys = require('./permissions.js');
+
+var knockknock = require('knock-knock-jokes')
+
+console.log(knockknock()) // returns a knock knock joke
+
+var T = new twit(keys);
+
+// T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
+//   console.log(data)
+// })
+
+// T.get('users/show', { screen_name: 'NicholasMalone5'}, function(err, data, response) {
+// 	console.log(data);
+// })
+
+var stream = T.stream('statuses/filter', {follow: ['3510233482','738767076']});
+
+stream.on('tweet',tweetEvent);
+
+function tweetEvent(eventMsg){
+	console.log("Got a tweet!");
+
+	var replyto = eventMsg.in_reply_to_screen_name;
+	var text = eventMsg.text;
+	var from = eventMsg.user.screen_name;
+
+	if(replyto === null){
+		var newTweet = '@' + from+ ' '+knockknock();
+		tweetIt(newTweet, eventMsg.id_str);
+	}
+}
+
+function tweetIt(txt, statusID){
+	var tweet = {
+		status: txt,
+		in_reply_to_status_id: statusID
+	}
+
+	T.post('statuses/update',tweet,tweeted);
+
+	function tweeted(err, data, response) {
+		if(err){
+			console.log("This went wrong! "+err);
+		}else{
+			console.log("It worked!");
+		}
+	}
+}
---
+++ permissions.js
@@ -0,0 +1,6 @@
+module.exports={
+  consumer_key:         'TYJPG92TSZm4w65A3C07ZaSan',
+  consumer_secret:      'vBDKscIMzmU0ewIttcC5M5lMpayl5IRlbhmop5Y2Jy659rG3T2',
+  access_token:         '808774125904941060-afT8zPaXjVDI4GE5ksRdm2XgyZBoSbX',
+  access_token_secret:  'GqDpx0H3ioDWFSEXrMUdG5jmyrGBpkYGABx4YOWXfJNCO',
+}
