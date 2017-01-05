19015d90ca27642ecba5813dfe25751d725c1b8e
Nick Behrens <behrensn@iastate.edu>
2017-01-05 00:46:00 -0600

    Added Functonality to make a song by using specific chars in tweets, gets data from the USA. Analysis and gathering are in different parts

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 console.log('Its alive');
 
+var tweets ={};
+var numberOfTweets = 0;
+
 var twit = require('twit');
 
 var keys = require('./permissions.js');
@@ -8,25 +11,206 @@ var knockknock = require('knock-knock-jokes');
 
 var MonkeyLearn = require('monkeylearn');
 
+var fs = require('fs');
+
+const scribble = require('scribbletune');
+
 var ml = new MonkeyLearn('0209052bc1311824816e8f4773e40ba740b2cc0d');
 
-console.log(knockknock()) // returns a knock knock joke
+//console.log(knockknock()) // returns a knock knock joke
 
 var T = new twit(keys);
+/*Parsing the Tweet File when we have data and want to get musical :)
+parseTweetFile();
 
-// T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
-//   console.log(data)
-// })
+function parseTweetFile(){
+	console.log("Starting to parse file");
+	fs.readFile('tweetDataForUSA.json', 'utf8', function (err, data) {
+  		if (err) throw err;
+  		obj = JSON.parse(data);
+		parseTweetDataJSON(obj);
+	});
+}
 
-// T.get('users/show', { screen_name: 'NickBehrens'}, function(err, data, response) {
-// 	console.log(data);
-// })
-// T.get('users/show', { screen_name: 'jackcoleman5'}, function(err, data, response) {
-// 	console.log(data);
-// })
-//
+function parseTweetDataJSON(data){
+	var i =0;
+	var notesData = [];
+	console.log("Starting to parse through data");
+	while(data[i]!=undefined){
+		var sharp = true;
+		console.log("Data at "+i+" is: "+data[i].text);
+		var text = data[i].text+"";
+		var noteNum = i%11;
+		for(var j=0;j<text.length;j++){
+			var char = text.charAt(j);
+			if(char == 'a'){
+				if(sharp){
+					notesData.push('a'+noteNum);
+				}else{
+					notesData.push('a#'+noteNum);
+				}
+			}
+			if(char == 'A'){
+				if(sharp){
+					notesData.push('Amaj');
+				}else{
+					notesData.push('A#maj');
+				}
+			}
+			if(char == 'b'){
+				if(sharp){
+					notesData.push('a'+noteNum);
+				}else{
+					notesData.push('a#'+noteNum);
+				}
+			}
+			if(char == 'B'){
+				if(sharp){
+					notesData.push('Bmaj');
+				}else{
+					notesData.push('B#maj');
+				}
+			}
+			if(char == 'c'){
+				if(sharp){
+					notesData.push('c'+noteNum);
+				}else{
+					notesData.push('c#'+noteNum);
+				}
+			}
+			if(char == 'C'){
+				if(sharp){
+					notesData.push('Cmaj');
+				}else{
+					notesData.push('C#maj');
+				}
+			}
+			if(char == 'd'){
+				if(sharp){
+					notesData.push('d'+noteNum);
+				}else{
+					notesData.push('d#'+noteNum);
+				}
+			}
+			if(char == 'D'){
+				if(sharp){
+					notesData.push('Dmaj');
+				}else{
+					notesData.push('D#maj');
+				}
+			}
+			if(char == 'e'){
+				if(sharp){
+					notesData.push('e'+noteNum);
+				}else{
+					notesData.push('e#'+noteNum);
+				}
+			}
+			if(char == 'E'){
+				if(sharp){
+					notesData.push('Emaj');
+				}else{
+					notesData.push('E#maj');
+				}
+			}
+			if(char == 'f'){
+				if(sharp){
+					notesData.push('f'+noteNum);
+				}else{
+					notesData.push('f#'+noteNum);
+				}
+			}
+			if(char == 'F'){
+				if(sharp){
+					notesData.push('Fmaj');
+				}else{
+					notesData.push('F#maj');
+				}
+			}
+			if(char == 'g'){
+				if(sharp){
+					notesData.push('g'+noteNum);
+				}else{
+					notesData.push('g#'+noteNum);
+				}
+			}
+			if(char == 'G'){
+				if(sharp){
+					notesData.push('Gmaj');
+				}else{
+					notesData.push('G#maj');
+				}
+			}
+			if(sharp){
+				sharp = false;
+			}else{
+				sharp = true;
+			}
+		}
+
+		var from = data[i].from;
+		i++;
+	}
+	console.log(notesData);
+	var clipNum = 0;
+	var spliceStart =0;
+	var spliceEnd = 15;
+	var numberOfNotes = 0;
+	var data = JSON.stringify(notesData,null,2);
+	fs.writeFile("NotesData.json",data);
+	while(numberOfNotes < notesData.length) {
+		var notesForBar = notesData.slice(spliceStart,spliceEnd);
+		fs.appendFile("NotesForBar",JSON.stringify(notesForBar,null,2));
+		let clip = scribble.clip({
+			notes: notesForBar,
+			pattern: 'x_x_x_--',
+			sizzle:true
+		});
+		scribble.midi(clip,"clip"+clipNum+".mid");
+		clipNum++;
+		if(spliceEnd+15>notesData.length){
+			spliceEnd = notesData.length;
+		}else{
+			spliceEnd = spliceEnd + 15;
+		}
+		spliceStart = spliceStart + 15;
+		numberOfNotes = numberOfNotes + 15;
+	}
+	// combineMidiFiles(clipNum-1);
+}
+
+function combineMidiFiles(clipNum){
+	var i=0;
+	var finalMidi = "";
+	while(i<clipNum){
+		fs.readFile('clip'+i+'.mid','utf8', function (err, data) {
+			fs.appendFile("finalMidi.mid",data);
+		});
+		fs.unlink('clip'+i+'.mid');
+		i++;
+	}
+	fs.writeFile("finalMidi.mid",finalMidi);
+}
+*/
+
+
+/*Typical Post Status with Twit
+T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
+  console.log(data)
+})
+*/
+
+/*Show Data From a User
+T.get('users/show', { screen_name: 'NickBehrens'}, function(err, data, response) {
+	console.log(data);
+})
+T.get('users/show', { screen_name: 'jackcoleman5'}, function(err, data, response) {
+	console.log(data);
+})
+*/
+
 /* Reply to people every time they tweet */
-var stream = T.stream('statuses/filter', {follow: ['419216928','3510233482','738767076']});
+ var stream = T.stream('statuses/filter', {follow: ['419216928','3510233482','738767076']});
 
 stream.on('tweet',tweetEvent);
 
@@ -40,8 +224,40 @@ function tweetEvent(eventMsg){
 		tweetIt(newTweet, eventMsg.id_str);
 	}
 }
-/*Tweet by getting images from a site */
-//TODO
+/*Get Data For all tweets in the USA
+var USA = [ '-124.25', '26.26', '-58.68', '48.33' ];
+
+var streamUS = T.stream('statuses/filter', { locations: USA });
+
+streamUS.on('tweet',tweetEventUS);
+
+function tweetEventUS(eventMsg){
+	var text = [eventMsg.text];
+	var from = eventMsg.user.screen_name;
+	var object = {from:eventMsg.user.screen_name,text:eventMsg.text};
+	tweets[numberOfTweets] = object;
+	console.log("Got a tweet from "+from+"!");
+	var jsonData = JSON.stringify(tweets,null,2);
+	fs.writeFile("tweetDataForUSA.json", jsonData);
+}
+*/
+
+/*Analize Text Setiment with MonkeyLearnvar
+module_id = 'cl_qkjxv9Ly';
+var p = ml.classifiers.classify(module_id, text, true);
+
+p.then(function (res) {
+	if(res.result[0][0].label=="positive"){
+		numberOfPositive++;
+	}
+	if(res.result[0][0].label=="neutral"){
+		numberOfMeh++;
+	}
+	if(res.result[0][0].label=="negative"){
+		numberOfNegative++;
+	}
+});
+*/
 
 /*Analize tweets sent to us and reply based on their setiment*/
 //TODO
